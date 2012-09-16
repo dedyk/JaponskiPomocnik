31d231f39f3c445fe0e058d98dfb9090765d8256
dedyk <dedykjp@gmail.com>
2012-09-16 18:08:03 +0200

    Test ze znakow kanji

--- src/pl/idedyk/android/japaneselearnhelper/JapaneseAndroidLearnHelperMainActivity.java
+++ src/pl/idedyk/android/japaneselearnhelper/JapaneseAndroidLearnHelperMainActivity.java
@@ -10,6 +10,7 @@ import pl.idedyk.android.japaneselearnhelper.kana.Kana;
 import pl.idedyk.android.japaneselearnhelper.kana.KanaTestOptions;
 import pl.idedyk.android.japaneselearnhelper.kanji.KanjiSearch;
 import pl.idedyk.android.japaneselearnhelper.kanji.hkr.KanjiRecognizeActivity;
+import pl.idedyk.android.japaneselearnhelper.kanji.hkr.KanjiTestOptionsActivity;
 import pl.idedyk.android.japaneselearnhelper.problem.ReportProblem;
 
 import android.app.Activity;
@@ -23,133 +24,143 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class JapaneseAndroidLearnHelperMainActivity extends Activity {
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.main);
-        
-        // create menu
-        createMenuList();
-        
-        // init menu actions
-        initMenuActions();
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		// create menu
+		createMenuList();
+
+		// init menu actions
+		initMenuActions();
+	}
 
 	private void createMenuList() {
-    	ListView mainMenuListView = (ListView)findViewById(R.id.mainMenuList);
-    	
-    	List<MainMenuItem> mainMenuListItems = new ArrayList<MainMenuItem>();
-    	
-    	mainMenuListItems.add(new MainMenuItem(
-    			getString(R.string.main_menu_kana_kanji),
-    			getString(R.string.main_menu_kana_text)));
-    	
-    	mainMenuListItems.add(new MainMenuItem(
-    			getString(R.string.main_menu_kana_test_kanji),
-    			getString(R.string.main_menu_kana_test_text)));    	
-    	
-    	mainMenuListItems.add(new MainMenuItem(
-    			getString(R.string.main_menu_dictionary_kanji),
-    			getString(R.string.main_menu_dictionary_text)));
-    	
-    	/*
+		ListView mainMenuListView = (ListView)findViewById(R.id.mainMenuList);
+
+		List<MainMenuItem> mainMenuListItems = new ArrayList<MainMenuItem>();
+
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_kana_kanji),
+				getString(R.string.main_menu_kana_text)));
+
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_kana_test_kanji),
+				getString(R.string.main_menu_kana_test_text)));    	
+
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_dictionary_kanji),
+				getString(R.string.main_menu_dictionary_text)));
+
+		/*
     	mainMenuListItems.add(new MainMenuItem(
     			getString(R.string.main_menu_word_test_kanji),
     			getString(R.string.main_menu_word_test_text)));
-    	
+
     	mainMenuListItems.add(new MainMenuItem(
     			getString(R.string.main_menu_kanji_test_kanji),
     			getString(R.string.main_menu_kanji_test_text)));
-    	*/
+		 */
 
-    	mainMenuListItems.add(new MainMenuItem(
-    			getString(R.string.main_menu_kanji_kanji),
-    			getString(R.string.main_menu_kanji_text)));
-    	
-    	mainMenuListItems.add(new MainMenuItem(
-    			getString(R.string.main_menu_kanji_recognizer_kanji),
-    			getString(R.string.main_menu_kanji_recognizer_text)));
-    	
-    	mainMenuListItems.add(new MainMenuItem(
-    			getString(R.string.main_menu_suggestion_kanji),
-    			getString(R.string.main_menu_suggestion_text)));
-    	
-    	mainMenuListItems.add(new MainMenuItem(
-    			getString(R.string.main_menu_information_kanji),
-    			getString(R.string.main_menu_information_text)));
-    	
-    	MainMenuListItemAdapter mainMenuListItemsAdapter = new MainMenuListItemAdapter(this, R.layout.main_menu_simplerow, mainMenuListItems);
-    	
-    	mainMenuListView.setAdapter(mainMenuListItemsAdapter);
-    }
-	
-    private void initMenuActions() {
-    	
-    	final ListView mainMenuListView = (ListView)findViewById(R.id.mainMenuList);
-    	
-    	mainMenuListView.setOnItemClickListener(new OnItemClickListener() {
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_kanji_kanji),
+				getString(R.string.main_menu_kanji_text)));
+
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_kanji_recognizer_kanji),
+				getString(R.string.main_menu_kanji_recognizer_text)));
+
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_kanji_test_kanji),
+				getString(R.string.main_menu_kanji_test_text)));
+
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_suggestion_kanji),
+				getString(R.string.main_menu_suggestion_text)));
+
+		mainMenuListItems.add(new MainMenuItem(
+				getString(R.string.main_menu_information_kanji),
+				getString(R.string.main_menu_information_text)));
+
+		MainMenuListItemAdapter mainMenuListItemsAdapter = new MainMenuListItemAdapter(this, R.layout.main_menu_simplerow, mainMenuListItems);
+
+		mainMenuListView.setAdapter(mainMenuListItemsAdapter);
+	}
+
+	private void initMenuActions() {
+
+		final ListView mainMenuListView = (ListView)findViewById(R.id.mainMenuList);
+
+		mainMenuListView.setOnItemClickListener(new OnItemClickListener() {
 
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				
+
 				if (position == 0) { // kana selected
 					Intent intent = new Intent(getApplicationContext(), Kana.class);
-					
+
 					startActivity(intent);
 				} else if (position == 1) { // kana test selected 
 					Intent intent = new Intent(getApplicationContext(), KanaTestOptions.class);
-					
+
 					startActivity(intent);
 				} else if (position == 2) { // dictionary selected
 					Intent intent = new Intent(getApplicationContext(), WordDictionary.class);
-					
+
 					startActivity(intent);
 				} 
 				/*
 				else if (position == X) { // word test selected		
 					Intent intent = new Intent(getApplicationContext(), WordTestGroup.class);
-					
+
 					startActivity(intent);
 				}
-					*/
+				 */
 				else if (position == 3) { // kanji search
 					Intent intent = new Intent(getApplicationContext(), KanjiSearch.class);
-					
+
 					startActivity(intent);
 				} 
-				
-				else if (position == 4) { // kanji recognizer
-					
-					Intent intent = new Intent(getApplicationContext(), KanjiRecognizeActivity.class);
-					
-					startActivity(intent);					
-					
-				} else if (position == 5) { // suggestion
-					String chooseEmailClientTitle = getString(R.string.choose_email_client);
-					
-					String mailSubject = getString(R.string.main_menu_email_subject);
-					
-					String mailBody = getString(R.string.main_menu_email_body);
-					
-			        String versionName = "";
-			        int versionCode = 0;
-			        
-			        try {
-			        	PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-			        	
-			            versionName = packageInfo.versionName;
-			            versionCode = packageInfo.versionCode;
 
-			        } catch (NameNotFoundException e) {        	
-			        }
-					
+				else if (position == 4) { // kanji recognizer
+
+					Intent intent = new Intent(getApplicationContext(), KanjiRecognizeActivity.class);
+
+					startActivity(intent);					
+
+				} else if (position == 5) { // kanji test
+
+					Intent intent = new Intent(getApplicationContext(), KanjiTestOptionsActivity.class);
+
+					startActivity(intent);					
+
+				} else if (position == 6) { // suggestion
+					String chooseEmailClientTitle = getString(R.string.choose_email_client);
+
+					String mailSubject = getString(R.string.main_menu_email_subject);
+
+					String mailBody = getString(R.string.main_menu_email_body);
+
+					String versionName = "";
+					int versionCode = 0;
+
+					try {
+						PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+
+						versionName = packageInfo.versionName;
+						versionCode = packageInfo.versionCode;
+
+					} catch (NameNotFoundException e) {        	
+					}
+
 					Intent reportSuggestionIntent = ReportProblem.createReportProblemIntent(mailSubject, mailBody, versionName, versionCode); 
-					
+
 					startActivity(Intent.createChooser(reportSuggestionIntent, chooseEmailClientTitle));
-				} else if (position == 6) { // info
+				} else if (position == 7) { // info
 					Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
-					
+
 					startActivity(intent);
 				}
 			}
@@ -159,7 +170,7 @@ public class JapaneseAndroidLearnHelperMainActivity extends Activity {
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		
+
 		DictionaryManager.getInstance().close();
 	}
 }
---
+++ src/pl/idedyk/android/japaneselearnhelper/kanji/hkr/KanjiTestOptionsActivity.java
@@ -0,0 +1,15 @@
+package pl.idedyk.android.japaneselearnhelper.kanji.hkr;
+
+import pl.idedyk.android.japaneselearnhelper.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KanjiTestOptionsActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.kanji_test_options);
+	}
+}
