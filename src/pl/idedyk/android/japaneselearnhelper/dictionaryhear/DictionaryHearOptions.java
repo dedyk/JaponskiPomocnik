d3e4f3bc88877b1974a645a91087b1260ce84876
dedyk <dedykjp@gmail.com>
2014-08-13 21:02:01 +0200

    Poprawka inicjalizacji TTS'a w opcjach sluchania slowek

--- src/pl/idedyk/android/japaneselearnhelper/dictionaryhear/DictionaryHearOptions.java
+++ src/pl/idedyk/android/japaneselearnhelper/dictionaryhear/DictionaryHearOptions.java
@@ -37,6 +37,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class DictionaryHearOptions extends Activity {
+	
+	private TtsConnector japanaeseTtsConnector;
+	
+	private TtsConnector polishTtsConnector;
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -111,268 +115,266 @@ public class DictionaryHearOptions extends Activity {
 		final Button startButton = (Button) findViewById(R.id.dictionary_hear_start);
 
 		// check TTS
-		final ProgressDialog progressDialog = ProgressDialog.show(this,
-				getString(R.string.dictionary_hear_options_check_tts1),
-				getString(R.string.dictionary_hear_options_check_tts2));
-
-		class TtsInitResult {
-
-			public boolean japaneseTtsResult;
-
-			public boolean polishTtsResult;
+		if (japanaeseTtsConnector != null) {
+			japanaeseTtsConnector.stop();
 		}
+		
+		japanaeseTtsConnector = new TtsConnector(DictionaryHearOptions.this, TtsLanguage.JAPANESE);
 
-		class PrepareAsyncTask extends AsyncTask<Void, Void, TtsInitResult> {
-
-			@Override
-			protected TtsInitResult doInBackground(Void... params) {
-
-				TtsInitResult ttsInitResult = new TtsInitResult();
-
-				TtsConnector japanaeseTtsConnector = new TtsConnector(DictionaryHearOptions.this, TtsLanguage.JAPANESE);
-				TtsConnector polishTtsConnector = new TtsConnector(DictionaryHearOptions.this, TtsLanguage.POLISH);
-
-				boolean japaneseInitialized = isTtsConnectorInitialized(japanaeseTtsConnector);
-				boolean polishInitialized = isTtsConnectorInitialized(polishTtsConnector);
-
-				ttsInitResult.japaneseTtsResult = japaneseInitialized;
-				ttsInitResult.polishTtsResult = polishInitialized;
-
-				japanaeseTtsConnector.stop();
-				polishTtsConnector.stop();
-
-				return ttsInitResult;
-			}
-
-			private boolean isTtsConnectorInitialized(TtsConnector ttsConnector) {
-
-				for (int idx = 0; idx < 10; ++idx) {
-
-					Boolean onInitResult = ttsConnector.getOnInitResult();
-
-					if (onInitResult == null) {
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-						}
-
-					} else {
-
-						return onInitResult.booleanValue();
-					}
-				}
-
-				return false;
-			}
-
-			@SuppressWarnings("deprecation")
-			@Override
-			protected void onPostExecute(TtsInitResult ttsInitResult) {
-				super.onPostExecute(ttsInitResult);
-
-				progressDialog.dismiss();
-
-				// INFO: Tylko do testow
-				//				int warning = 0;
-				//				ttsInitResult.japaneseTtsResult = true;
-				//				ttsInitResult.polishTtsResult = true;
-
-				if (ttsInitResult.japaneseTtsResult == true && ttsInitResult.polishTtsResult == true) {
-					return;
-				}
-
-				AlertDialog alertDialog = new AlertDialog.Builder(DictionaryHearOptions.this).create();
-
-				String message = null;
-
-				if (ttsInitResult.japaneseTtsResult == false && ttsInitResult.polishTtsResult == false) {
-					message = getString(R.string.tts_japanese_polish_error);
-				} else if (ttsInitResult.japaneseTtsResult == true && ttsInitResult.polishTtsResult == false) {
-					message = getString(R.string.tts_polish_error);
-				} else if (ttsInitResult.japaneseTtsResult == false && ttsInitResult.polishTtsResult == true) {
-					message = getString(R.string.tts_japanese_error);
-				} else {
-					throw new RuntimeException();
-				}
-
-				alertDialog.setMessage(message);
-				alertDialog.setCancelable(false);
-
-				alertDialog.setButton(getString(R.string.tts_error_ok), new DialogInterface.OnClickListener() {
-
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						// noop
-					}
-				});
-
-				if (ttsInitResult.japaneseTtsResult == false) {
-					alertDialog.setButton2(getString(R.string.tts_svox_google_play_go),
-							new DialogInterface.OnClickListener() {
-
-								@Override
-								public void onClick(DialogInterface dialog, int which) {
-
-									Uri marketUri = Uri.parse(getString(R.string.tts_svox_market_url));
-
-									Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
-
-									startActivity(marketIntent);
-								}
-							});
-				}
-
-				if (ttsInitResult.polishTtsResult == false) {
-					alertDialog.setButton3(getString(R.string.tts_ivona_google_play_go),
-							new DialogInterface.OnClickListener() {
-
-								@Override
-								public void onClick(DialogInterface dialog, int which) {
-
-									Uri marketUri = Uri.parse(getString(R.string.tts_ivona_market_url));
-
-									Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
-
-									startActivity(marketIntent);
-								}
-							});
-				}
-
-				alertDialog.show();
-
-				startButton.setEnabled(false);
-			}
+		if (polishTtsConnector != null) {
+			polishTtsConnector.stop();
 		}
-
-		new PrepareAsyncTask().execute();
-
+		
+		polishTtsConnector = new TtsConnector(DictionaryHearOptions.this, TtsLanguage.POLISH);
+			
 		// start action		
 		startButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
+				
+				final ProgressDialog progressDialog = ProgressDialog.show(DictionaryHearOptions.this,
+						getString(R.string.dictionary_hear_options_check_tts1),
+						getString(R.string.dictionary_hear_options_check_tts2));
 
-				String repeatNumberString = repeatNumberEditText.getText().toString();
+				class PrepareAsyncTask extends AsyncTask<Void, Void, Void> {
 
-				boolean repeatNumberError = false;
-
-				int repeatNumber = -1;
-
-				if (repeatNumberString == null) {
-					repeatNumberError = true;
-				} else {
-
-					try {
-						repeatNumber = Integer.parseInt(repeatNumberString);
-					} catch (NumberFormatException e) {
-						repeatNumberError = true;
+					private boolean japaneseInitialized = false;
+					private boolean polishInitialized = false;
+					
+					@Override
+					protected Void doInBackground(Void... params) {
+						
+						japaneseInitialized = isTtsConnectorInitialized(japanaeseTtsConnector);
+						polishInitialized = isTtsConnectorInitialized(polishTtsConnector);
+						
+						return null;
 					}
-				}
 
-				if (repeatNumberError == false && repeatNumber <= 0) {
-					repeatNumberError = true;
-				}
+					private boolean isTtsConnectorInitialized(TtsConnector ttsConnector) {
 
-				if (repeatNumberError == true) {
+						for (int idx = 0; idx < 10; ++idx) {
 
-					Toast toast = Toast.makeText(DictionaryHearOptions.this,
-							getString(R.string.dictionary_hear_options_repeat_number_invalid), Toast.LENGTH_SHORT);
+							Boolean onInitResult = ttsConnector.getOnInitResult();
 
-					toast.show();
+							if (onInitResult == null) {
+								try {
+									Thread.sleep(1000);
+								} catch (InterruptedException e) {
+								}
 
-					return;
-				}
+							} else {
 
-				dictionaryHearConfig.setRepeatNumber(repeatNumber);
-
-				String delayNumberString = delayNumberEditText.getText().toString();
-
-				boolean delayNumberError = false;
-
-				int delayNumber = -1;
-
-				if (delayNumberString == null) {
-					delayNumberError = true;
-				} else {
-
-					try {
-						delayNumber = Integer.parseInt(delayNumberString);
-					} catch (NumberFormatException e) {
-						delayNumberError = true;
-					}
-				}
-
-				if (delayNumberError == false && delayNumber < 0) {
-					delayNumberError = true;
-				}
-
-				if (delayNumberError == true) {
-
-					Toast toast = Toast.makeText(DictionaryHearOptions.this,
-							getString(R.string.dictionary_hear_options_delay_number_invalid), Toast.LENGTH_SHORT);
-
-					toast.show();
-
-					return;
-				}
-
-				dictionaryHearConfig.setDelayNumber(delayNumber);
-
-				boolean random = randomCheckBox.isChecked();
-
-				dictionaryHearConfig.setRandom(random);
-
-				List<DictionaryEntry> chosenAllDictionaryEntryList = new ArrayList<DictionaryEntry>();
-
-				List<GroupEnum> chosenWordGroupsNumberList = new ArrayList<GroupEnum>();
-
-				for (int wordGroupCheckBoxListIdx = 0; wordGroupCheckBoxListIdx < wordGroupCheckBoxList.size(); ++wordGroupCheckBoxListIdx) {
-
-					CheckBox currentWordGroupCheckBox = wordGroupCheckBoxList.get(wordGroupCheckBoxListIdx);
-
-					if (currentWordGroupCheckBox.isChecked() == true) {
-
-						List<DictionaryEntry> currentWordsGroupDictionaryEntryList = JapaneseAndroidLearnHelperApplication
-								.getInstance().getDictionaryManager(DictionaryHearOptions.this)
-								.getGroupDictionaryEntries(groupsNames.get(wordGroupCheckBoxListIdx));
-
-						for (int repeatIdx = 0; repeatIdx < repeatNumber; ++repeatIdx) {
-							chosenAllDictionaryEntryList.addAll(currentWordsGroupDictionaryEntryList);
+								return onInitResult.booleanValue();
+							}
 						}
 
-						chosenWordGroupsNumberList.add(groupsNames.get(wordGroupCheckBoxListIdx));
+						return false;
+					}
+
+					@SuppressWarnings("deprecation")
+					@Override
+					protected void onPostExecute(Void voidNull) {
+						
+						super.onPostExecute(voidNull);
+
+						progressDialog.dismiss();
+
+						// INFO: Tylko do testow
+						//				int warning = 0;
+						//				japaneseInitialized = true;
+						//				polishInitialized = true;
+						
+						if (japaneseInitialized == false || polishInitialized == false) { // wystapil blad
+							
+							AlertDialog alertDialog = new AlertDialog.Builder(DictionaryHearOptions.this).create();
+
+							String message = null;
+
+							if (japaneseInitialized == false && polishInitialized == false) {
+								message = getString(R.string.tts_japanese_polish_error);
+							} else if (japaneseInitialized == true && polishInitialized == false) {
+								message = getString(R.string.tts_polish_error);
+							} else if (japaneseInitialized == false && polishInitialized == true) {
+								message = getString(R.string.tts_japanese_error);
+							} else {
+								throw new RuntimeException();
+							}
+
+							alertDialog.setMessage(message);
+							alertDialog.setCancelable(false);
+
+							alertDialog.setButton(getString(R.string.tts_error_ok), new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+									// noop
+								}
+							});
+
+							if (japaneseInitialized == false) {
+								alertDialog.setButton2(getString(R.string.tts_svox_google_play_go),
+										new DialogInterface.OnClickListener() {
+
+											@Override
+											public void onClick(DialogInterface dialog, int which) {
+
+												Uri marketUri = Uri.parse(getString(R.string.tts_svox_market_url));
+
+												Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+
+												startActivity(marketIntent);
+											}
+										});
+							}
+
+							if (polishInitialized == false) {
+								alertDialog.setButton3(getString(R.string.tts_ivona_google_play_go),
+										new DialogInterface.OnClickListener() {
+
+											@Override
+											public void onClick(DialogInterface dialog, int which) {
+
+												Uri marketUri = Uri.parse(getString(R.string.tts_ivona_market_url));
+
+												Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+
+												startActivity(marketIntent);
+											}
+										});
+							}
+
+							alertDialog.show();							
+							
+						} else { // ok
+							
+							// przygotowanie danych
+							
+							String repeatNumberString = repeatNumberEditText.getText().toString();
+
+							boolean repeatNumberError = false;
+
+							int repeatNumber = -1;
+
+							if (repeatNumberString == null) {
+								repeatNumberError = true;
+							} else {
+
+								try {
+									repeatNumber = Integer.parseInt(repeatNumberString);
+								} catch (NumberFormatException e) {
+									repeatNumberError = true;
+								}
+							}
+
+							if (repeatNumberError == false && repeatNumber <= 0) {
+								repeatNumberError = true;
+							}
+
+							if (repeatNumberError == true) {
+
+								Toast toast = Toast.makeText(DictionaryHearOptions.this,
+										getString(R.string.dictionary_hear_options_repeat_number_invalid), Toast.LENGTH_SHORT);
+
+								toast.show();
+
+								return;
+							}
+
+							dictionaryHearConfig.setRepeatNumber(repeatNumber);
+
+							String delayNumberString = delayNumberEditText.getText().toString();
+
+							boolean delayNumberError = false;
+
+							int delayNumber = -1;
+
+							if (delayNumberString == null) {
+								delayNumberError = true;
+							} else {
+
+								try {
+									delayNumber = Integer.parseInt(delayNumberString);
+								} catch (NumberFormatException e) {
+									delayNumberError = true;
+								}
+							}
+
+							if (delayNumberError == false && delayNumber < 0) {
+								delayNumberError = true;
+							}
+
+							if (delayNumberError == true) {
+
+								Toast toast = Toast.makeText(DictionaryHearOptions.this,
+										getString(R.string.dictionary_hear_options_delay_number_invalid), Toast.LENGTH_SHORT);
+
+								toast.show();
+
+								return;
+							}
+
+							dictionaryHearConfig.setDelayNumber(delayNumber);
+
+							boolean random = randomCheckBox.isChecked();
+
+							dictionaryHearConfig.setRandom(random);
+
+							List<DictionaryEntry> chosenAllDictionaryEntryList = new ArrayList<DictionaryEntry>();
+
+							List<GroupEnum> chosenWordGroupsNumberList = new ArrayList<GroupEnum>();
+
+							for (int wordGroupCheckBoxListIdx = 0; wordGroupCheckBoxListIdx < wordGroupCheckBoxList.size(); ++wordGroupCheckBoxListIdx) {
+
+								CheckBox currentWordGroupCheckBox = wordGroupCheckBoxList.get(wordGroupCheckBoxListIdx);
+
+								if (currentWordGroupCheckBox.isChecked() == true) {
+
+									List<DictionaryEntry> currentWordsGroupDictionaryEntryList = JapaneseAndroidLearnHelperApplication
+											.getInstance().getDictionaryManager(DictionaryHearOptions.this)
+											.getGroupDictionaryEntries(groupsNames.get(wordGroupCheckBoxListIdx));
+
+									for (int repeatIdx = 0; repeatIdx < repeatNumber; ++repeatIdx) {
+										chosenAllDictionaryEntryList.addAll(currentWordsGroupDictionaryEntryList);
+									}
+
+									chosenWordGroupsNumberList.add(groupsNames.get(wordGroupCheckBoxListIdx));
+								}
+							}
+
+							if (chosenAllDictionaryEntryList.size() == 0) {
+
+								Toast toast = Toast.makeText(DictionaryHearOptions.this,
+										getString(R.string.dictionary_hear_options_word_group_no_chosen), Toast.LENGTH_SHORT);
+
+								toast.show();
+
+								return;
+							}
+
+							dictionaryHearConfig.setChosenWordGroups(GroupEnum.convertToValues(chosenWordGroupsNumberList));
+
+							if (random == true) {
+								Collections.shuffle(chosenAllDictionaryEntryList);
+							}
+
+							JapaneseAndroidLearnHelperDictionaryHearContext dictionaryHearContext = JapaneseAndroidLearnHelperApplication
+									.getInstance().getContext().getDictionaryHearContext();
+
+							dictionaryHearContext.reset();
+
+							dictionaryHearContext.setDictionaryEntryList(chosenAllDictionaryEntryList);
+
+							finish();
+
+							Intent intent = new Intent(getApplicationContext(), DictionaryHear.class);
+
+							startActivity(intent);							
+						}						
 					}
 				}
 
-				if (chosenAllDictionaryEntryList.size() == 0) {
-
-					Toast toast = Toast.makeText(DictionaryHearOptions.this,
-							getString(R.string.dictionary_hear_options_word_group_no_chosen), Toast.LENGTH_SHORT);
-
-					toast.show();
-
-					return;
-				}
-
-				dictionaryHearConfig.setChosenWordGroups(GroupEnum.convertToValues(chosenWordGroupsNumberList));
-
-				if (random == true) {
-					Collections.shuffle(chosenAllDictionaryEntryList);
-				}
-
-				JapaneseAndroidLearnHelperDictionaryHearContext dictionaryHearContext = JapaneseAndroidLearnHelperApplication
-						.getInstance().getContext().getDictionaryHearContext();
-
-				dictionaryHearContext.reset();
-
-				dictionaryHearContext.setDictionaryEntryList(chosenAllDictionaryEntryList);
-
-				finish();
-
-				Intent intent = new Intent(getApplicationContext(), DictionaryHear.class);
-
-				startActivity(intent);
-
+				new PrepareAsyncTask().execute();
 			}
 		});
 
@@ -432,4 +434,19 @@ public class DictionaryHearOptions extends Activity {
 			}
 		});
 	}
+	
+	@Override
+	protected void onDestroy() {
+
+		if (japanaeseTtsConnector != null) {
+			japanaeseTtsConnector.stop();
+		}
+
+		if (polishTtsConnector != null) {
+			polishTtsConnector.stop();
+		}
+		
+		super.onDestroy();
+	}
+
 }
