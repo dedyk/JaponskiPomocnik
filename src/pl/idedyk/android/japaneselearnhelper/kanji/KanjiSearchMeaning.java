5e93494cab2e92d3ae043a0d417c68bf9dcf4bd9
dedyk <dedykjp@gmail.com>
2016-01-04 17:22:31 +0100

    Autocomplete

--- src/pl/idedyk/android/japaneselearnhelper/kanji/KanjiSearchMeaning.java
+++ src/pl/idedyk/android/japaneselearnhelper/kanji/KanjiSearchMeaning.java
@@ -37,7 +37,6 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.ScrollView;
