d89608267be0be24ce93ce299a5c6b0ec40bd96e
dedyk <dedykjp@gmail.com>
2016-08-15 20:00:31 +0200

    Nowa struktura

--- clean.sh
+++ clean.sh
@@ -2,10 +2,10 @@
 
 rm -rf db-lucene
 
-rm app/src/main/assets/kana.csv
-rm app/src/main/assets/radical.csv
-rm app/src/main/assets/transitive_intransitive_pairs.csv
-rm app/src/main/assets/word-power.csv
-rm app/src/main/assets/kanji_recognizer.model.db
-rm app/src/main/assets/dictionary.zip
+rm -f app/src/main/assets/kana.csv
+rm -f app/src/main/assets/radical.csv
+rm -f app/src/main/assets/transitive_intransitive_pairs.csv
+rm -f app/src/main/assets/word-power.csv
+rm -f app/src/main/assets/kanji_recognizer.model.db
+rm -f app/src/main/assets/dictionary.zip
 rm -rf release/
