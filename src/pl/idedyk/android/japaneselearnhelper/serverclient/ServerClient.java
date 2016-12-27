fb1c504fbc3793afb8288bb33732bcb727f30706
dedyk <dedykjp@gmail.com>
2016-12-27 17:44:52 +0100

    Zmiana namiarow na serwer oraz male poprawienie komunikatu

--- src/pl/idedyk/android/japaneselearnhelper/serverclient/ServerClient.java
+++ src/pl/idedyk/android/japaneselearnhelper/serverclient/ServerClient.java
@@ -40,7 +40,7 @@ import android.util.Log;
 public class ServerClient {
 	
 	//private static final String PREFIX_URL = "http://10.0.2.2:8080"; // dev
-	private static final String PREFIX_URL = "http://japonski-pomocnik.idedyk.pl"; // prod
+	private static final String PREFIX_URL = "http://www.japonski-pomocnik.pl"; // prod
 		
 	private static final String SEND_MISSING_WORD_URL = PREFIX_URL + "/android/sendMissingWord";
 	private static final String SEARCH_URL = PREFIX_URL + "/android/search";
