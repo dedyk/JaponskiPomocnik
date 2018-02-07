1be9cad4479b3f4b8ce1e7f7dd704aa856e5f1dd
dedyk <dedykjp@gmail.com>
2018-02-07 22:50:05 +0100

    Nowa struktura aplikacji

---
+++ generate-result-db.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#mkdir bin-db-generator
+
+japanese_dictionary_api_version=1.0-SNAPSHOT
+japanese_dictionary_lucene_common_version=1.0-SNAPSHOT
+lucene_version=4.7.2
+
+CLASSPATH=app/libs/javacsv-2.1.jar
+CLASSPATH=$CLASSPATH:app/libs/JapaneseDictionaryAPI-${japanese_dictionary_api_version}.jar
+CLASSPATH=$CLASSPATH:app/libs/JapaneseDictionaryLuceneCommon-${japanese_dictionary_lucene_common_version}.jar
+CLASSPATH=$CLASSPATH:app/libs/lucene-analyzers-common-${lucene_version}.jar
+CLASSPATH=$CLASSPATH:libs-db-generator/lucene-core-${lucene_version}.jar
+CLASSPATH=$CLASSPATH:libs-db-generator/lucene-suggest-${lucene_version}.jar
+
+java -cp $CLASSPATH pl.idedyk.japanese.dictionary.lucene.LuceneDBGenerator \
+android \
+db/word.csv \
+db/sentences.csv \
+db/sentences_groups.csv \
+db/kanji.csv \
+db/radical.csv \
+db/names.csv \
+db-lucene
+
+cp db/kana.csv app/src/main/assets/
+cp db/radical.csv app/src/main/assets/
+cp db/transitive_intransitive_pairs.csv app/src/main/assets/
+cp db/word-power.csv app/src/main/assets/
+cp db/kanji_recognizer.model.db app/src/main/assets/
+
+cd db-lucene
+zip -9 ../app/src/main/assets/dictionary.zip *
+cd ..
